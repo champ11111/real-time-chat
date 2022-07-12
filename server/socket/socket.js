e3f5c71fa5ada1f136c5e2a051e350a79507d2bb
champ11111 <champ_2543@hotmail.com>
2022-07-12 14:53:04 +0700

    change socket function to emit message to room

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,20 +1,23 @@
 exports.socketIo = (io) => {
+  console.log("Socket open");
   io.on("connection", (socket) => {
-    socket.on("setup", (userData) => {
-      socket.join(userData._id);
+    socket.on("setup", (user) => {
+      socket.join(user._id);
       socket.emit("connected");
     });
 
     socket.on("join room", (room) => {
+      console.log(`user join room id ${room}`);
       socket.join(room);
     });
 
     socket.on("new message", (receivedMessage) => {
       const room = receivedMessage.room;
-      room.users.forEach((user) => {
-        if (user == receivedMessage.sender._id) return;
-        socket.in(user).emit("message recieved", receivedMessage);
-      });
+      // room.users.forEach((user) => {
+      //   if (user === receivedMessage.sender._id) return;
+      //   socket.in(user).emit("message received", receivedMessage);
+      // });
+      socket.to(room._id).emit("message received", receivedMessage);
     });
 
     socket.off("setup", () => {
